a6bdbb5ec51c4a41fc5641242af1d771f712fed7
Henk van den Berg <henk.van.den.berg@dans.knaw.nl>
2016-11-07 16:33:19 +0100

    Make project dependent on rspub-core

---
+++ setup.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='rspub-gui',
+    version='0.1',
+    packages=[],
+    url='https://github.com/EHRI/rspub-gui',
+    license='Apache License 2.0',
+    author='henk van den berg',
+    author_email='henk.van.den.berg at dans.knaw.nl',
+    description='Application for ResourceSync publishing',
+    install_requires=['rspub-core']
+)
+
+
