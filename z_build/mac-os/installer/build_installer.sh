99cd9d63439664e447e6fe28d846a893fea1cac5
Henk van den Berg <henk.van.den.berg@dans.knaw.nl>
2017-02-24 17:18:42 +0100

    Create Mac Installer

---
+++ z_build/mac-os/installer/build_installer.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# working with Qt Installer Framework from
+# https://download.qt.io/official_releases/qt-installer-framework/2.0.3/QtInstallerFramework-mac-x64.dmg
+
+current_mpt_version="MetadataPublishingTool-1.0.rc.4.app"
+current_qt_version="QtIFW2.0.3"
+
+qt_bin=~/Qt/$current_qt_version/bin/binarycreator
+qt_archivegen=~/Qt/$current_qt_version/bin/archivegen
+
+if [ -e "$qt_bin" ]; then
+    echo "Found $qt_bin"
+else
+    echo "No Qt Installer Framework found at $qt_bin"
+fi
+
+$qt_archivegen packages/nl.knaw.dans.ehri.mpt/data/mpt.7z ../dist/$current_mpt_version
+
+$qt_bin -c config/config.xml -p packages MPT-Installer
