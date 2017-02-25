0898ec28adaac663468e55abeefba8f83096d8ed
henk van den berg <henkvandenberg8@gmail.com>
2017-02-25 15:17:30 +0100

    Modify win installer

--- z_build/win7_onefile/installer/build_installer.bat
+++ z_build/win7_onefile/installer/build_installer.bat
@@ -1,8 +1,6 @@
 @echo off
 title Build a Windows installer with Qt Installer Framework
 
-rem NOT WORKING. The application works fine, once installed it gives a fatal error.
-
 rem https://download.qt.io/official_releases/qt-installer-framework/2.0.3/QtInstallerFramework-win-x86.exe
 
 SET current_mpt_version="MetadataPublishingTool-1.0.rc.4.exe"
