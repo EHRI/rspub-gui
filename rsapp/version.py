83d7babcd6b347581d1ca182e5ef8858b7dfc4cf
Henk van den Berg <henk.van.den.berg@dans.knaw.nl>
2017-02-27 19:13:16 +0100

    Add about and versioning 3

--- rsapp/version.py
+++ rsapp/version.py
@@ -3,6 +3,6 @@ __release_date__ = "2017-02-27"
 __about__ = "<p>Metadata Publishing Tool is an application for publishing resources " \
             "in conformance with the " \
             "<a href='http://www.openarchives.org/rs/1.1/resourcesync'>ResourceSync Framework Specification</a>.</p>" \
-            "<p>Metadata Publishing Tool was developed under auspices of the <b/>" \
+            "<p>Metadata Publishing Tool was developed under auspices of the <br></br>" \
             "<a href='https://www.ehri-project.eu/'>European Holocaust Research Infrastructure (EHRI)</a>.</p>" \
             "<p>Home page: <a href='https://github.com/EHRI/rspub-gui'>https://github.com/EHRI/rspub-gui</a></p>"
