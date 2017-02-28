ca1b65897cfd3452a8cc35b0984293ff96a290ba
Henk van den Berg <henk.van.den.berg@dans.knaw.nl>
2017-02-28 19:04:01 +0100

    Update i18n 2

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 from setuptools import setup
-
-version = {}
-with open("rsapp/version.py") as fp:
-    exec(fp.read(), version)
-# later on we use: version['__version__']
+from rsapp import version
 
 setup(
     name='rspub-gui',
-    version=version['__version__'],
+    version=version.__version__,
     packages=['rsapp.gui'],
     url='https://github.com/EHRI/rspub-gui',
     license='Apache License 2.0',
