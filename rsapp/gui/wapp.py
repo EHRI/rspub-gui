1245fb0893d760c46531d5fe03f725e65ea1f186
henk van den berg <henkvandenberg8@gmail.com>
2017-04-01 16:22:57 +0200

    Specify windows application icon

--- rsapp/gui/wapp.py
+++ rsapp/gui/wapp.py
@@ -1,7 +1,10 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 import logging.config
+import os
+import platform
 
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
 
 from rsapp.gui.ctrl import Ctrl
@@ -19,6 +22,14 @@ class RsApplication(QApplication):
         self.logger.info("application_home=%s" % self.ctrl.application_home)
         self.logger.info("locale_dir=%s" % self.ctrl.locale_dir)
         self.logger.info(_("Language was set to locale en-US"))
+
+        opsys = platform.system()
+        if opsys == "Windows":
+            self.logger.info("Setting Windows-specific values")
+            self.setWindowIcon(QIcon(os.path.join(application_home, "conf", "img", "mpt256.ico")))
+            import ctypes
+            mpt_app_id = "nl.knaw.dans.ehri2.mpt"
+            ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(mpt_app_id)
 
         self.main_window = WMain()
         self.aboutToQuit.connect(self.__before_close__)
